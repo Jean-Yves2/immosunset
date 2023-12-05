1365ef99efd389f76730a7f4cc4e0668978fc71c
Jean-Yves <sarakajean1@gmail.com>
2023-12-05 16:20:41 +0100

    add : dynamique card

--- src/app/components/house-card/house-card.component.ts
+++ src/app/components/house-card/house-card.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./house-card.component.scss']
 })
 export class HouseCardComponent {
-
+  homeList = [
+    { tile: 'Cottage', subTitle: 'Ensoleillé',image:"chambre-spacieuse3", avatar: 'avatar', description: 'Une retraite champêtre entourée de nature et de couleurs florales' },
+    { tile: 'Villa', subTitle: 'Océane',image:"vision-du-monde", avatar: 'avatar', description: 'Une oasis contemporaine offrant une vue infinie sur les vagues océaniques.' },
+    { tile: 'Refuge', subTitle: 'Montagnard',image:"terrasse-luxieuse", avatar: 'avatar', description: 'Un havre alpin au charme authentique avec des panoramas à couper le souffle.' },
+    { tile: 'Espace Urbain', subTitle: 'Éclatant',image:"villa-design-cannes", avatar: 'avatar', description: "Un loft citadin où l'architecture industrielle rencontre la lumière naturelle." }
+  ];
 }
